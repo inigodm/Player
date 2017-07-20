4bd84eaf530d3863af57a85a69d8adc137f40169
inigodm <inigo.delgado@gmail.com>
2017-07-20 22:56:28 +0200

    Testing

--- app/src/androidTest/java/com/inigo/player/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/inigo/player/ExampleInstrumentedTest.java
@@ -1,11 +1,18 @@
 package com.inigo.player;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.inigo.player.logics.tasks.playlistload.SongsLoader;
+import com.inigo.player.models.TitleSubtitle;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +28,9 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
         assertEquals("com.inigo.player", appContext.getPackageName());
-
+        List<TitleSubtitle> songs = new ArrayList<>();
+        SongsLoader sl = new SongsLoader(appContext.getContentResolver(), songs);
+        sl.run();
+        assertTrue(songs.size() > 0);
     }
 }
