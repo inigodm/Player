d8d7a215273739dc1af1aab97bd4bf6c23ea8226
inigodm <inigo.delgado@gmail.com>
2017-10-29 09:06:55 +0100

    Song playing

--- app/src/main/java/com/inigo/player/models/models.kt
+++ app/src/main/java/com/inigo/player/models/models.kt
@@ -1,9 +1,16 @@
 package com.inigo.player.models
 
+import kotlin.reflect.KProperty
+
 /**
  * Created by inigo on 8/09/17.
  */
 data class Artist(val id: Int = 0, val name: String = "")
 data class Album(val id: Int = 0, val name: String = "")
-data class PlayList(val id: Int, val name: String)
 data class PlayListSongs (val id_playlist: Int, val id_song: Int)
+
+/*class PlayList {
+    var name: String  = ""
+    var id: Int = 0
+    var id_type: Int = 0
+}*/
